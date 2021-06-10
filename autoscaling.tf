978ed3ff720b674d55eabe2781176dfd6c3f4f52
Stephen J. Butler <stephen.butler@gmail.com>
2021-06-10 11:41:42 -0500

    Add encrypted root volume

--- autoscaling.tf
+++ autoscaling.tf
@@ -156,6 +156,8 @@ resource "aws_launch_template" "bastion" {
             delete_on_termination = true
             volume_size           = 30
             volume_type           = "gp3"
+
+            encrypted = true
         }
     }
 
