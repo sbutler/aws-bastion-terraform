1e8b7645ef1cc35610c314937ff83bac51a961a7
Stephen J. Butler <stephen.butler@gmail.com>
2021-06-16 15:58:23 -0500

    Add _providers.tf file

---
+++ _providers.tf
@@ -0,0 +1,53 @@
+terraform {
+    required_version = "~> 1.0.0"
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 3.44"
+        }
+        cloudinit = {
+            source  = "hashicorp/cloudinit"
+            version = "~> 2.2"
+        }
+        time = {
+            source  = "hashicorp/time"
+            version = "~> 0.7.1"
+        }
+        null = {
+            source  = "hashicorp/null"
+            version = "~> 3.1"
+        }
+    }
+
+    /* CHANGEME: Uncomment to use as a standalone
+    backend "s3" {
+        bucket         = "deploy-bastion-CHANGEME"
+        key            = "bastion/state.tfstate"
+        dynamodb_table = "terraform"
+
+        encrypt    = true
+
+        region = "us-east-2"
+    }
+    */
+
+    experiments = [ module_variable_optional_attrs ]
+}
+
+/* CHANGEME: Uncomment to use as a standalone
+provider "aws" {
+    region = "us-east-2"
+    default_tags {
+        tags = local.default_tags
+    }
+}
+*/
+
+locals {
+    default_tags = {
+        Service     = var.service
+        Contact     = var.contact
+        Environment = var.environment
+        Project     = var.project
+    }
+}
